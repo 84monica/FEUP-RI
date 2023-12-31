a8bfaa1d07f9e7826c0e9ae71954212c8106667a
84monica <up201905753@fe.up.pt>
2023-12-31 15:00:00 +0000

    Add stop sign detection and implement stop functionality

--- src/control.py
+++ src/control.py
@@ -33,6 +33,9 @@ env.render()
 
 total_reward = 0
 
+stop_sign = False
+stop_steps = 0
+
 while True:
 
     lane_pose = env.get_lane_pos2(env.cur_pos, env.cur_angle)
@@ -45,7 +48,16 @@ while True:
     k_d = 10
     
     # The speed is a value between [0, 1] (which corresponds to a real speed between 0m/s and 1.2m/s)
-    speed = 0.3
+    if stop_sign:
+        speed = 0
+        stop_steps += 1
+        
+        if stop_steps > 30:
+            stop_sign = False
+            stop_steps = 0
+            
+    else:
+        speed = 0.3
     
     # Angle of the steering wheel, which corresponds to the angular velocity in rad/s
     steering = k_p*distance_to_road_center + k_d*angle_from_straight_in_rads 
@@ -55,31 +67,40 @@ while True:
 
     # print('Steps = %s, Timestep Reward=%.3f, Total Reward=%.3f' % (env.step_count, reward, total_reward))
 
-    # Check for any signs every 50 steps
+    # Check for any signs
     
-    if env.step_count % 50 == 0:
-        print("Checking for signs...")
+    if env.step_count % 20 == 0:
+        print("Checking for signs...\n")
 
         # Convert the observation into an image
         im = Image.fromarray(obs)
         im.save("image.jpg")
-        
-        # [{'x': 50.5, 'y': 130.0, 'width': 29.0, 'height': 46.0, 
-        # 'confidence': 0.5285166501998901, 'class': 't_intersect', 
-        # 'class_confidence': None, 'class_id': 1, 'tracker_id': None, 
-        # 'image_path': 'image.jpg', 'prediction_type': 'ObjectDetectionModel'}]
 
         predictions = model.predict("image.jpg", confidence=40, overlap=30).json()["predictions"]
         
         if (predictions != []):
             type_of_sign = predictions[0]["class"]
-            distance_to_intersection = predictions[0]["x"]
+            x, y, w, h = predictions[0]["x"], predictions[0]["y"], predictions[0]["width"], predictions[0]["height"]
             confidence = predictions[0]["confidence"]
             
+            # Print the sign info
             if (type_of_sign == "t_intersect"):
-                print("Intersection ahead! \n Distance: " + str(distance_to_intersection) + "\n Confidence: " + str(confidence))
+                print("Intersection ahead! \n" + "X = " + str(x) + ", Y = " + str(y) + ", W = " + str(w) + ", H =" + str(h) + "\n Confidence: " + str(confidence) + "\n")
             elif (type_of_sign == "stop"):
-                print("Stop sign ahead! \n Distance: " + str(distance_to_intersection) + "\n Confidence: " + str(confidence))
+                print("Stop sign ahead! \n" + "X = " + str(x) + ", Y = " + str(y) + ", W = " + str(w) + ", H =" + str(h) + "\n Confidence: " + str(confidence) + "\n")
+            
+            # If the sign is a stop sign, 
+            # the confidence is high enough and 
+            # the stop sign is at the right side of the screen and is big on the screen then stop the car
+            if (confidence > 0.5):
+                if (type_of_sign == "stop"):
+                    # Sign is at right of the screen
+                    if (x > 300):
+                        # Sign is bigger than 100 pixels
+                        if (w > 100):
+                            print("STOP!!!!!! \n")
+                            stop_sign = True
+            
             
     env.render()
 
