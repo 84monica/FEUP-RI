eec3ba5f833645981f752e9a0d7cff4b64a66823
84monica <up201905753@fe.up.pt>
2023-12-31 21:26:45 +0000

    fix: Refractor and Add Coments

--- src/ai_city_driving.py
+++ src/ai_city_driving.py
@@ -5,19 +5,21 @@ from gym_duckietown.envs import DuckietownEnv
 import os
 from roboflow import Roboflow
 
-
+# Path to custom map
 current_dir = os.path.dirname(os.path.abspath(__file__))
 map_file = os.path.join(current_dir, 'map.yaml')
 
+# Initiatize Model
+rf = Roboflow(api_key="UUZRYYia676DD0a4CbI2")
+project = rf.workspace().project("ri-h9xbe")
+model = project.version(2).model
+
+# Initialize DuckieTown Simulation
 parser = argparse.ArgumentParser()
 parser.add_argument('--env-name', default=None)
 parser.add_argument('--map-name', default=map_file)
 parser.add_argument('--no-pause', action='store_true', help="don't pause on failure")
 args = parser.parse_args()
-
-rf = Roboflow(api_key="UUZRYYia676DD0a4CbI2")
-project = rf.workspace().project("ri-h9xbe")
-model = project.version(2).model
 
 if args.env_name is None:
     env = DuckietownEnv(
@@ -31,25 +33,16 @@ else:
 obs = env.reset()
 env.render()
 
-total_reward = 0
-
 stop_sign = False
 stop_steps = 0
 
 while True:
-
-    lane_pose = env.get_lane_pos2(env.cur_pos, env.cur_angle)
-    distance_to_road_center = lane_pose.dist
-    angle_from_straight_in_rads = lane_pose.angle_rad
-
-    # Calculate the speed and direction.
-
-    k_p = 20
-    k_d = 10
-    
-    # The speed is a value between [0, 1] (which corresponds to a real speed between 0m/s and 1.2m/s)
     if stop_sign:
+        # Is at stop sign so stop for 30 steps
+
         speed = 0
+        steering = 0
+        
         stop_steps += 1
         
         if stop_steps > 30:
@@ -57,31 +50,43 @@ while True:
             stop_steps = 0
             
     else:
+        # Calculate the speed and direction.        
+        
+        lane_pose = env.get_lane_pos2(env.cur_pos, env.cur_angle)
+        distance_to_road_center = lane_pose.dist
+        angle_from_straight_in_rads = lane_pose.angle_rad
+
+        k_p = 20
+        k_d = 10
+
+        # The speed is a value between [0, 1] (which corresponds to a real speed between 0m/s and 1.2m/s)
         speed = 0.3
     
-    # Angle of the steering wheel, which corresponds to the angular velocity in rad/s
-    steering = k_p*distance_to_road_center + k_d*angle_from_straight_in_rads 
+        # Angle of the steering wheel, which corresponds to the angular velocity in rad/s
+        steering = k_p*distance_to_road_center + k_d*angle_from_straight_in_rads 
     
+    # Make step
     obs, reward, done, info = env.step([speed, steering])
-    total_reward += reward
-
-    # print('Steps = %s, Timestep Reward=%.3f, Total Reward=%.3f' % (env.step_count, reward, total_reward))
+    # print('Steps = %s, Timestep Reward=%.3f, ' % (env.step_count, reward))
 
     # Check for any signs
-    
-    if env.step_count % 20 == 0:
+    if env.step_count % 50 == 0:
         print("Checking for signs...\n")
 
         # Convert the observation into an image
         im = Image.fromarray(obs)
         im.save("image.jpg")
 
+        # Make a prediction based on the image
         predictions = model.predict("image.jpg", confidence=40, overlap=30).json()["predictions"]
         
         if (predictions != []):
+            # Extract type of sign found in the image and confidence that it was found
             type_of_sign = predictions[0]["class"]
-            x, y, w, h = predictions[0]["x"], predictions[0]["y"], predictions[0]["width"], predictions[0]["height"]
             confidence = predictions[0]["confidence"]
+            
+            # Extract coords and size of where the sign was found in the image
+            x, y, w, h = predictions[0]["x"], predictions[0]["y"], predictions[0]["width"], predictions[0]["height"]
             
             # Print the sign info
             if (type_of_sign == "t_intersect"):
@@ -89,8 +94,7 @@ while True:
             elif (type_of_sign == "stop"):
                 print("Stop sign ahead! \n" + "X = " + str(x) + ", Y = " + str(y) + ", W = " + str(w) + ", H =" + str(h) + "\n Confidence: " + str(confidence) + "\n")
             
-            # If the sign is a stop sign, 
-            # the confidence is high enough and 
+            # If the sign is a stop sign, the confidence is high enough and 
             # the stop sign is at the right side of the screen and is big on the screen then stop the car
             if (confidence > 0.5):
                 if (type_of_sign == "stop"):
@@ -102,11 +106,16 @@ while True:
                             stop_sign = True
             
             
+    # Render Simulation
     env.render()
 
+    # In case it goes off the road or crashes
     if done:
         if reward < 0:
             print('*** CRASHED ***')
-        print ('Final Reward = %.3f' % total_reward)
+        else:
+            print('*** OFF ROAD ***')     
+
+        # Reset environment, respawn and continue simulation 
         env.reset()
         done = False
